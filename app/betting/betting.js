a77be41ebcc1ea5ea7c6d4194cfa795b69db1b6a
Vadim Nicolai <vadim.nicolai@endava.com>
2016-05-18 17:58:55 +0300

    Frontpage and predictions page: adapted to material design requirements.

--- app/betting/betting.js
+++ app/betting/betting.js
@@ -1,12 +1,6 @@
 'use strict';
 
-angular.module('myApp.betting', ['ngRoute'])
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/betting', {
-      templateUrl: 'betting/betting.html',
-      controller: 'bettingCtrl'
-    });
-  }])
+angular.module('myApp.betting', [])
 
   .controller('bettingCtrl', function ($scope, $http, $q) {
     var tokenHeader = {headers: {'X-Auth-Token': '822fca9c9da2416592e3e0a8ac86c239'}},
@@ -14,6 +8,6 @@ angular.module('myApp.betting', ['ngRoute'])
         teams = $http.get('http://api.football-data.org/v1/soccerseasons/406/teams', tokenHeader);
 
     $q.all([fixtures, teams]).then(function(arrayOfResults) {
-      $scope.contacts = arrayOfResults[0].data.fixtures.slice(-40).reverse();
+      $scope.predicts = arrayOfResults[0].data.fixtures.slice(-40).reverse();
     });
   });
