5aec5daf7f351fea363ebbe42ba0dc6763c33fc6
Vadim Nicolai <vadim.nicolai@endava.com>
2016-05-26 11:02:17 +0300

    Team page: added on click request for every team.

--- src/teams/teams.js
+++ src/teams/teams.js
@@ -1,26 +1,21 @@
- (function() {
-     'use strict';
+ 'use strict';
 
-     angular
-       .module('myApp')
-       .controller('Teams', Teams);
+ angular.module('myApp.teams', [])
 
-     Teams.$inject = ['$http', '$q'];
+   .controller('Teams', function ($scope, $http, $q) {
+     var tokenHeader = {headers: {'X-Auth-Token': '822fca9c9da2416592e3e0a8ac86c239'}},
+         teams = $http.get('http://api.football-data.org/v1/soccerseasons/398/teams', tokenHeader);
 
-     /* @ngInject */
-     function Teams($http, $q) {
-         var vm = this;
-         vm.teams = [];
+     teams.then(function(arrayOfResults) {
+       $scope.teams = arrayOfResults.data.teams;
+       console.log($scope.teams);
+     });
 
-         activate();
+     $scope.send = function(team){
+        console.log();
+        $http.get(team._links.players.href, tokenHeader)
+         .success(function(response) {$scope.players = response.players; });
 
-         function activate() {
-           var tokenHeader = {headers: {'X-Auth-Token': '822fca9c9da2416592e3e0a8ac86c239'}},
-               teams = $http.get('http://api.football-data.org/v1/soccerseasons/398/teams', tokenHeader);
+    };
 
-               teams.then(function(arrayOfResults) {
-                  vm.teams = arrayOfResults.data.teams;
-               });
-         }
-     }
- })();
+})
