48682745a4312e17ffab63deaf2f22af8345a850
Vadim Nicolai <vadim.nicolai@endava.com>
2016-05-26 14:40:13 +0300

    Team page: exposing players href to team listing.

--- src/teams/teams.js
+++ src/teams/teams.js
@@ -1,21 +1,47 @@
- 'use strict';
+'use strict';
 
- angular.module('myApp.teams', [])
+angular.module('myApp.teams', [])
 
-   .controller('Teams', function ($scope, $http, $q) {
-     var tokenHeader = {headers: {'X-Auth-Token': '822fca9c9da2416592e3e0a8ac86c239'}},
-         teams = $http.get('http://api.football-data.org/v1/soccerseasons/398/teams', tokenHeader);
+.controller('Teams', function ($scope, $http, $q, $mdDialog) {
+  var tokenHeader = {headers: {'X-Auth-Token': '822fca9c9da2416592e3e0a8ac86c239'}},
+  teams = $http.get('http://api.football-data.org/v1/soccerseasons/398/teams', tokenHeader);
 
-     teams.then(function(arrayOfResults) {
-       $scope.teams = arrayOfResults.data.teams;
-       console.log($scope.teams);
-     });
+  teams.then(function(arrayOfResults) {
+    $scope.teams = arrayOfResults.data.teams;
+  });
 
-     $scope.send = function(team){
-        console.log();
-        $http.get(team._links.players.href, tokenHeader)
-         .success(function(response) {$scope.players = response.players; });
+  function showTabDialog() {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'teams/dialog.html',
+      parent: angular.element(document.body),
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
 
-    };
-
+  $scope.send = function(team){
+    $http.get(team._links.players.href, tokenHeader)
+    .success(function(response) {
+      $scope.players = response.players;
+      showTabDialog();
+    });
+  };
 })
+
+function DialogController($scope, $mdDialog) {
+  $scope.test = 'test';
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
