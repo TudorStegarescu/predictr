787fd65f224b842451568131dc3db0f60752268a
Vadim Nicolai <vadim.nicolai@endava.com>
2016-05-27 12:55:07 +0300

    Team page: team players exposed via material dialog. TBD: replace rootScope approach to service based solution.

--- src/teams/teams.js
+++ src/teams/teams.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp.teams', [])
 
-.controller('Teams', function ($scope, $http, $q, $mdDialog) {
+.controller('Teams', function ($rootScope, $scope, $http, $q, $mdDialog) {
   var tokenHeader = {headers: {'X-Auth-Token': '822fca9c9da2416592e3e0a8ac86c239'}},
   teams = $http.get('http://api.football-data.org/v1/soccerseasons/398/teams', tokenHeader);
 
@@ -27,14 +27,14 @@ angular.module('myApp.teams', [])
   $scope.send = function(team){
     $http.get(team._links.players.href, tokenHeader)
     .success(function(response) {
-      $scope.players = response.players;
+      $rootScope.players = response.players;
       showTabDialog();
     });
   };
 })
 
-function DialogController($scope, $mdDialog) {
-  $scope.test = 'test';
+function DialogController($rootScope, $scope, $mdDialog) {
+
   $scope.hide = function() {
     $mdDialog.hide();
   };
