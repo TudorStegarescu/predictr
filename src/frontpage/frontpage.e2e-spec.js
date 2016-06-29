6fb0b578a904478b876d928d4652f35e37a768f5
Vadim Nicolai <vadim.nicolai@endava.com>
2016-06-29 09:55:25 +0300

    Protractor: added e2e test that check ng-repeat and create a screenshot.

---
+++ src/frontpage/frontpage.e2e-spec.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+
+function writeScreenShot(data, filename) {
+  var stream = fs.createWriteStream(filename);
+
+  stream.write(new Buffer(data, 'base64'));
+  stream.end();
+}
+
+  describe('Protractor Demo App', function() {
+
+    it('should add one and two', function() {
+
+      browser.get('http://localhost:8000/src/#/frontpage');
+
+      var el = element(by.css('.test'));
+      el.getText().then(function(text) {
+        console.log(text);
+      });
+
+      expect(element(by.exactRepeater('contact in contacts')).isPresent())
+      .toBe(true);
+
+      browser.takeScreenshot().then(function (png) {
+        writeScreenShot(png, 'protractor-screenshot.png');
+      });
+
+  });
+
+});
